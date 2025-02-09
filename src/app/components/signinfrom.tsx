c2b20a926e73d42a9ed453d77620eb2c6855501f
Navkirat singh <132665095+Vanillapapa64@users.noreply.github.com>
2025-02-09 21:47:38 +0530

    signin loading added

--- src/app/components/signinfrom.tsx
+++ src/app/components/signinfrom.tsx
@@ -16,16 +16,17 @@ export function SignInForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false)
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault(); // Prevent default form submission behavior
     setError('');
-
+    setLoading(true)
     const res = await signIn("credentials", {
       username,
       password,
       redirect: false,
     });
-
+    setLoading(false)
     if (res?.error) {
       setError('Invalid username or password');
     } else {
@@ -87,7 +88,7 @@ export function SignInForm() {
           type="submit"
           className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Sign in
+          {loading ? "Signing in..." : "Sign in"}
         </Button>
       </div>
     </form>
